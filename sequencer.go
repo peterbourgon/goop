2dc2b0ea8212643ded15ed1aecf29349bb28635b
Peter Bourgon <peter@bourgon.org>
2012-02-02 17:39:43 +0100

    Sequencer: try 1

---
+++ sequencer.go
@@ -0,0 +1,84 @@
+package goop
+
+import (
+	"fmt"
+)
+
+type Slot []Event
+
+// A Sequencer is a set of Events which are fired to a set of 
+// EventReceivers on a schedule determined by the Clock.
+type Sequencer struct {
+	eventIn   chan Event
+	receivers []EventReceiver
+	slots     []Slot
+	pos       int
+}
+
+func (s *Sequencer) Events() chan<- Event { return s.eventIn }
+
+func (s *Sequencer) String() string {
+	return fmt.Sprintf("%d receivers, %d slots", len(s.receivers), len(s.slots))
+}
+
+func NewSequencer() *Sequencer {
+	ei := make(chan Event, OTHER_CHAN_BUFFER)
+	sl := make([]Slot, 0)
+	re := make([]EventReceiver, 0)
+	s := &Sequencer{ei, re, sl, 0}
+	go s.sequenceLoop()
+	return s
+}
+
+func (s *Sequencer) register(r EventReceiver) {
+	for _, existing := range s.receivers {
+		if existing == r {
+			return
+		}
+	}
+	s.receivers = append(s.receivers, r)
+}
+
+func (s *Sequencer) unregister(r EventReceiver) {
+	for i, existing := range s.receivers {
+		if r == existing {
+			s.receivers = append(s.receivers[:i], s.receivers[:i+1]...)
+			return	
+		}
+	}
+}
+
+func (s *Sequencer) sequenceLoop() {
+	for {
+		select {
+		case ev := <-s.eventIn:
+			switch ev.Name {
+			case "kill":
+				return
+			case "reset":
+				s.pos = 0
+				s.receivers = make([]EventReceiver, 0)
+			case "register":
+				if r, ok := ev.Arg.(EventReceiver); ok {
+					s.register(r)	
+				}
+			case "unregister":
+				if r, ok := ev.Arg.(EventReceiver); ok {
+					s.unregister(r)
+				}
+			case "tick":
+				if len(s.slots) > 0 {
+					for _, r := range s.receivers {
+						for _, ev := range s.slots[s.pos] {
+							r.Events() <- ev
+						}
+					}
+				}
+				s.pos++
+				if s.pos >= len(s.slots) {
+					s.pos = 0
+				}
+			}
+		}
+	}
+}
