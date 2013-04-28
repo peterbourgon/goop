12c8e9b72ce95f730b5d9dd09da231b7841810ee
Peter Bourgon <peter@bourgon.org>
2013-04-28 16:49:53 +0200

    Fresh start

--- audio-sender.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type AudioSender interface {
-	AudioOut() <-chan []float32
-	Reset() // stop playback by breaking downstream connections
-}
--- debug.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func D(format string, args ...interface{}) {
-	fmt.Printf("DEBUG "+format+"\n", args...)
-}
